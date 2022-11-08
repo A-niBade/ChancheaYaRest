758c9fa0e3a6954d2848d97c32be928620926ffb
José Maldonado <joan.maldonadoa@gmail.com>
2022-11-08 17:00:47 -0300

    Servicios de CATEGORIAPOSTRE creados

---
+++ src/main/java/com/chanchearest/repo/CategoriaPostreRepo.java
@@ -0,0 +1,9 @@
+package com.chanchearest.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chanchearest.entities.CategoriaPostre;
+
+public interface CategoriaPostreRepo extends JpaRepository<CategoriaPostre, Integer> {
+
+}
